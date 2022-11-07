ce46848706e134b3a6d531b758c8f933d434d2f5
Callum Rollo <c.rollo@outlook.com>
2022-11-07 13:58:04 +0100

    oxy and chl flags

--- flag_qartod.py
+++ flag_qartod.py
@@ -18,7 +18,7 @@ pyglider_cotede_var_names = {
 temperature_vars = ["temperature"]
 salinity_vars = ["salinity", "conductivity"]
 cond_temp_vars = ["potential_density", "density", "potential_temperature"]
-secondary_vars = ["oxygen_concentration"]
+secondary_vars = ["oxygen_concentration", "chlorophyll"]
 vars_to_flag = temperature_vars + salinity_vars + cond_temp_vars + secondary_vars
 
 temp_config = {
@@ -43,15 +43,33 @@ salinity_config = {
 
 tempsal_config = {**temp_config, **salinity_config}
 
+oxygen_config = {
+    "oxygen_concentration": {
+        "qartod": {
+            "gross_range_test": {"suspect_span": [0, 350], "fail_span": [0, 500]},
+            "spike_test": {"suspect_threshold": 10, "fail_threshold": 50},
+            "location_test": {"bbox": [10, 50, 25, 60]},
+        }
+    }
+}
+
+
+chl_config = {
+    "chlorophyll": {
+        "qartod": {
+            "gross_range_test": {"suspect_span": [-1, 10], "fail_span": [-1, 15]},
+            "spike_test": {"suspect_threshold": 1, "fail_threshold": 5},
+            "location_test": {"bbox": [10, 50, 25, 60]},
+        }
+    }
+}
+
 
 def apply_ioos_flags(ds, config):
     c = Config(config)
     qc = XarrayStream(ds, lon="longitude", lat="latitude")
     runner = list(qc.run(c))
     results = collect_results(runner, how='list')
-    for res in results:
-        print(res.test)
-        print(f"result {res.results}")
     agg = CollectedResult(
         stream_id='',
         package='qartod',
@@ -78,6 +96,12 @@ def flag_ioos(ds):
     cond_temp_flags, cond_temp_flag_comment = apply_ioos_flags(ds, tempsal_config)
     combi_flagged_prop = 100 * sum(np.logical_and(cond_temp_flags > 1, cond_temp_flags < 9)) / len(cond_temp_flags)
     print(f"Flagged {combi_flagged_prop.round(5)} % of values derived from temperature and salinity as bad")
+    oxy_flags, oxy_flag_comment = apply_ioos_flags(ds, oxygen_config)
+    oxy_flagged_prop = 100 * sum(np.logical_and(oxy_flags > 1, oxy_flags < 9)) / len(oxy_flags)
+    print(f"Flagged {oxy_flagged_prop.round(5)} % of oxygen as bad")
+    chl_flags, chl_flag_comment = apply_ioos_flags(ds, chl_config)
+    chl_flagged_prop = 100 * sum(np.logical_and(chl_flags > 1, chl_flags < 9)) / len(chl_flags)
+    print(f"Flagged {chl_flagged_prop.round(5)} % of chlorophyll as bad")
     # Apply flags and add comment
     for name_pyglider in vars_to_flag:
         if name_pyglider in temperature_vars:
@@ -98,7 +122,16 @@ def flag_ioos(ds):
                            f"{ioos_qc.__version__}. Threshold values from EuroGOOS DATA-MEQ Working Group (2010)" \
                            f" Recommendations for in-situ data Near Real Time Quality Control [Version 1.2]. EuroGOOS" \
                            f", 23pp. DOI http://dx.doi.org/10.25607/OBP-214. Using config: {cond_temp_flag_comment} "
+        elif name_pyglider == "oxygen_concentration":
+            flag = cond_temp_flags
+            ioos_comment = f"Quality control flags from IOOS QC QARTOD https://github.com/ioos/ioos_qc Version: " \
+                           f"{ioos_qc.__version__}. Using config: {oxy_flag_comment} "
+        elif name_pyglider == "chlorophyll":
+            flag = cond_temp_flags
+            ioos_comment = f"Quality control flags from IOOS QC QARTOD https://github.com/ioos/ioos_qc Version: " \
+                           f"{ioos_qc.__version__}. Using config: {chl_flag_comment} "
         else:
+            print(f"no flags found for {name_pyglider}")
             continue
 
         ds[f"{name_pyglider}_quality_control"].values = flag
